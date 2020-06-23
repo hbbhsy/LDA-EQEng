3c2d20b31c05dafb1bebb8334beb3ec379f03a51
hbbhsy <mss_hsy@hotmail.com>
2020-06-23 15:49:00 -0700

    upload wordcloud

--- src/pipeline.py
+++ src/pipeline.py
@@ -4,6 +4,7 @@ import pandas as pd
 import string
 import sys
 import os
+import boto3
 from os import listdir
 from os.path import isfile, join
 import unicodedata
@@ -15,3 +16,11 @@ from nltk.tokenize import word_tokenize
 from nltk.stem.wordnet import WordNetLemmatizer
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
+
+#Loading Data From s3
+
+
+
+
+
+if __name__ == '__main__':
